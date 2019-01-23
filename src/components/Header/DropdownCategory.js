60ccbdb06e964c6e92b72fcf060fcbd44acac421
smithwebtek <brad@smithwebtek.com>
2019-01-22 19:38:32 -0500

    Stash Jan15 code

--- src/components/Header/DropdownCategory.js
+++ src/components/Header/DropdownCategory.js
@@ -1,64 +1,64 @@
 import React, { Component } from 'react';
 import {
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+	Dropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem
+} from 'reactstrap';
 import styles from './DropdownCategory.module.css';
 
-
 class DropdownCategory extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: false,
-      value : "Categories",
-      activeItem: []
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			dropdownOpen: false,
+			value: "Categories",
+			activeItem: []
+		};
+	}
 
 
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
-  }
+	toggle() {
+		this.setState({
+			dropdownOpen: !this.state.dropdownOpen
+		});
+	}
 
-  handleClick(cat, index) {
-    this.props.handleEvent(cat, "category");
-    if(index === -1) this.setState({
-      activeItem:[]
-    });
-    this.state.activeItem.includes(index) ?  this.setState({
-      activeItem : this.state.activeItem.filter( selected => selected !== index)
-    })
-    : this.state.activeItem.push(index);
-  }
+	handleClick(cat, index) {
+		this.props.handleEvent(cat, "category");
+		if (index === -1) this.setState({
+			activeItem: []
+		});
+		this.state.activeItem.includes(index) ? this.setState({
+			activeItem: this.state.activeItem.filter(selected => selected !== index)
+		})
+			: this.state.activeItem.push(index);
+	}
 
-  categoryMenuItems() {
-    return this.props.category.map((cat, index) =>
-    <DropdownItem toggle={false} onClick = {() => this.handleClick(cat, index)} key={cat}>
-    {this.state.activeItem.includes(index) ? <span>&#10004; {cat}</span>: cat}</DropdownItem>);
-  }
+	categoryMenuItems() {
+		return this.props.category.map((cat, index) =>
+			<DropdownItem toggle={false} onClick={() => this.handleClick(cat, index)} key={cat}>
+				{this.state.activeItem.includes(index) ? <span>&#10004; {cat}</span> : cat}</DropdownItem>);
+	}
 
-  render() {
-    return (
-      <div>
-        <Dropdown toggle = {this.toggle} isOpen={this.state.dropdownOpen} inNavbar>
-          <DropdownToggle nav caret>Category</DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem onClick = {() => this.handleClick("Clear", -1)} key={"Clear"}>Clear</DropdownItem>
-            <DropdownItem divider/>
-            <div className={styles.dropdownCategoryDiv}>
-              { this.categoryMenuItems() }
-            </div>
-          </DropdownMenu>
-        </Dropdown>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Dropdown toggle={this.toggle} isOpen={this.state.dropdownOpen} inNavbar>
+					<DropdownToggle nav caret>Category</DropdownToggle>
+					<DropdownMenu right>
+						<DropdownItem onClick={() => this.handleClick("Clear", -1)} key={"Clear"}>Clear</DropdownItem>
+						<DropdownItem divider />
+						<div className={styles.dropdownCategoryDiv}>
+							{this.categoryMenuItems()}
+						</div>
+					</DropdownMenu>
+				</Dropdown>
+			</div>
+		);
+	}
 }
 
 export default DropdownCategory;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,58 +1,60 @@
 import React, { Component } from 'react';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  Button} from 'reactstrap';
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	Button
+} from 'reactstrap';
 
 import SearchBar from '../Header/SearchBar.js';
 import CClargelogo from '../Header/Images/CClargelogo.png';
+import styles from './Header.module.css'
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+		this.toggleNavbar = this.toggleNavbar.bind(this);
+		this.state = {
+			collapsed: true
+		};
+	}
 
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+	toggleNavbar() {
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	}
 
-  render() {
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-        <NavbarBrand className="Logo-icon" href="/">
-          <img src={CClargelogo} alt="Community Connect logo"/>
-        </NavbarBrand>
-        <NavbarToggler onClick={this.toggleNavbar}  />
-        <Collapse isOpen={!this.state.collapsed} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-              <SearchBar type="text" handleFilter={this.props.handleFilter} />
-              </NavItem>
-              <NavItem>
-              <Button
-                color="secondary"
-                onClick={() => this.props.toggleSavedResourcesPane()}>
-                Saved Resources
+	render() {
+		return (
+			<div>
+				<Navbar color="light" light expand="md">
+					<NavbarBrand className="Logo-icon" href="/">
+						<img src={CClargelogo} className={styles.navheader} alt="Community Connect logo" />
+					</NavbarBrand>
+					<NavbarToggler onClick={this.toggleNavbar} />
+					<Collapse isOpen={!this.state.collapsed} navbar>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<SearchBar type="text" handleFilter={this.props.handleFilter} />
+							</NavItem>
+							<NavItem>
+								<Button
+									color="secondary"
+									onClick={() => this.props.toggleSavedResourcesPane()}>
+									Saved Resources
               </Button>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+							</NavItem>
+						</Nav>
+					</Collapse>
+				</Navbar>
+			</div>
+		);
+	}
 }
 
 export default Header;
